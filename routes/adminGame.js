d3b763526ab725f59589a1f3be3acb2f0ea427ac
bhl105337 <1059870337@qq.com>
2018-06-11 14:57:27 +0800

    Undersigned: bhl105337 <1059870337@qq.com>

--- routes/adminGame.js
+++ routes/adminGame.js
@@ -36,6 +36,8 @@ router.get('/gameAdmin', function (req, res, next) {
 router.get('/addGameMsg', function (req, res, next) {
     var data = req.query;
     var date = new Date();
+    var cls = 0;
+    var tag = 0;
 
     if (data.gameName) {
         game.hasGame(data.gameName, function (result) {
@@ -58,8 +60,8 @@ router.get('/addGameMsg', function (req, res, next) {
                     gameDetail: data.gameDetail || null,
                     admin: data.admin,
                     type: data.type,
-                    cls_ids: data.cls,
-                    // tag_ids: data.tag
+                    cls_ids: data.cls ? data.cls : cls,
+                    tag_ids: data.tag ? data.tag : tag
                 };
                 game.addGameMsg(gameMsg, function (result) {
                     console.log(result.insertId);
