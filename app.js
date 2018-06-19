e3b18a0f9644057f4bea8341a228d6ac40e2df5a
bhl105337 <1059870337@qq.com>
2018-06-19 09:26:37 +0800

    Undersigned: bhl105337 <1059870337@qq.com>

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var fs=require("fs");
+var fs = require("fs");
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var timeout = require('connect-timeout');
 var crypto = require('crypto');
 var md5 = crypto.createHash('md5');
-var multer =require('multer');
+var multer = require('multer');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var game = require('./routes/game');
@@ -22,52 +22,42 @@ var adminH5 = require('./routes/adminH5');
 var adminGame = require('./routes/adminGame');
 var adminStrategy = require("./routes/adminStrategy");
 var app = express();
-<<<<<<< HEAD
-app.use('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers","X-Requested-With,content-type");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1');
-    //res.header("Content-Type", "application/json;charset=utf-8");
-    //res.header("Content-Type", "text/html");
-=======
-
-app.all('*', function (req, res, next) {
+app.use('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With,content-type");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("X-Powered-By", ' 3.2.1');
-    res.header("Content-Type", "application/json;charset=utf-8");
+    //res.header("Content-Type", "application/json;charset=utf-8");
     //res.header("Content-Type", "text/html");
-    next();
-});
-app.use(function getIp(req, res, next) {
-    var ip = req.headers['x-forwarded-for'] ||
-        req.ip ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress || '';
-    if (ip.split(',').length > 0) {
-        ip = ip.split(',')[0];
-    }
-    console.log(ip);
-    //return ip;
->>>>>>> 529fa27c8b6d2e537d3b372a9c1be3b91c1f097d
-    next();
-});
-app.use(function(req,res,next){
-   if(req.url.indexOf("www/upload")!==-1){
-     fs.readFile("./"+req.url,function(err,data){
-        if(err){
-        }else{
-          res.write(data);
-          res.end();
-        }
-     });
 
-   }else{
-     next();
-   }
+    next();
+});
+//app.use(function getIp(req, res, next) {
+//    var ip = req.headers['x-forwarded-for'] ||
+//        req.ip ||
+//        req.connection.remoteAddress ||
+//        req.socket.remoteAddress ||
+//        req.connection.socket.remoteAddress || '';
+//    if (ip.split(',').length > 0) {
+//        ip = ip.split(',')[0];
+//    }
+//    console.log(ip);
+//    //return ip;
+//    next();
+//});
+app.use(function (req, res, next) {
+    if (req.url.indexOf("www/upload") !== -1) {
+        fs.readFile("./" + req.url, function (err, data) {
+            if (err) {
+            } else {
+                res.write(data);
+                res.end();
+            }
+        });
+
+    } else {
+        next();
+    }
 
 });
 //http://192.168.0.104:8878/upload/Stragey_IMG_15042018_091317_0.png
@@ -82,7 +72,7 @@ app.use(function(req,res,next){
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-var objMulter=multer({dest:"./www/upload/"});
+var objMulter = multer({dest: "./www/upload/"});
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
