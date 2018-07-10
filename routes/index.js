bd4471711877ceb79e37de3bdc31a53e824721a0
bhl105337 <1059870337@qq.com>
2018-07-10 11:42:30 +0800

    Undersigned: bhl105337 <1059870337@qq.com>

--- routes/index.js
+++ routes/index.js
@@ -6,34 +6,8 @@ var index = require("../DAO/index");
 router.get('/', function (req, res, next) {
     var test = 1;
     var date = new Date();
-    //console.log(text.html());
-    index.hasNewsComment(function (result) {
-        var newTime = "";
-        for (var i = 0; i < result.length; i++) {
-            if (i >= result.length) break;
-            var timeymd = result[i].add_time.substring(0, 10);
-            var timehis = result[i].add_time.substring(11, 19);
-
-            var timehis_a = timehis.split("-")
-            var newHis = ""
-            for (var ta in timehis_a) {
-                // if (ta >= timehis_a.length) continue;
-                newHis += timehis_a[ta] + ":";
-                // console.log(newHis);
-                //
-            }
-            newTime = timeymd + " " + newHis;
-            newTime = newTime.substring(0, newTime.length - 1);
-
-            index.upDateTime(result[i].id, newTime, function (up_result) {
-
-            })
-        }
-        res.json({s: 1})
-    })
-
     // index.carousel(1,function (result) {
-    //     res.json(result)
+    res.json({s: 1})
     // });
 
     //res.header('Content-Type', 'text/html');
