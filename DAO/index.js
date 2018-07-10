40972b46c71ab1b1628df15d05f79bb9b4e3a423
bhl105337 <1059870337@qq.com>
2018-07-10 11:36:07 +0800

    Undersigned: bhl105337 <1059870337@qq.com>

--- DAO/index.js
+++ DAO/index.js
@@ -11,6 +11,18 @@ var index = {
             return callback(result)
         })
     },
+    hasNewsComment: function (callback) {
+        var sql = "SELECT * FROM t_news_comment"
+        query(sql, [], function (result) {
+            return callback(result)
+        })
+    },
+    upDateTime: function (id, time, callback) {
+        var sql = "UPDATE t_news_comment SET add_time=? WHERE id=?";
+        query(sql, [time, id], function (result) {
+            return callback(result)
+        })
+    }
 };
 
 module.exports = index;
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,30 @@ router.get('/', function (req, res, next) {
     var test = 1;
     var date = new Date();
     //console.log(text.html());
-    res.json({s: parseInt(date.getTime() / 1000)})
+    index.hasNewsComment(function (result) {
+        var newTime = "";
+        for (var i = 0; i < result.length; i++) {
+            if (i >= result.length) break;
+            var timeymd = result[i].add_time.substring(0, 10);
+            var timehis = result[i].add_time.substring(11, 19);
+
+            var timehis_a = timehis.split("-")
+            var newHis = ""
+            for (var ta in timehis_a) {
+                // if (ta >= timehis_a.length) continue;
+                newHis += timehis_a[ta] + ":";
+                // console.log(newHis);
+                //
+            }
+            newTime = timeymd + " " + newHis;
+            newTime = newTime.substring(0, newTime.length - 1);
+
+            index.upDateTime(result[i].id, newTime, function (up_result) {
+
+            })
+        }
+        res.json({s: 1})
+    })
 
     // index.carousel(1,function (result) {
     //     res.json(result)
@@ -16,6 +39,5 @@ router.get('/', function (req, res, next) {
     //res.header('Content-Type', 'text/html');
     // res.render('index', { title: 'Express' });
 });
-
 
 module.exports = router;
