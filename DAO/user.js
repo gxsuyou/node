4cc2eda967c363f12e47c70e3d98e9568b49a77d
bhl105337 <1059870337@qq.com>
2018-10-09 14:14:31 +0800

    Undersigned: bhl105337 <1059870337@qq.com>

--- DAO/user.js
+++ DAO/user.js
@@ -2,206 +2,72 @@
  * Created by Administrator on 2016/12/15.
  */
 var query = require('../config/config');
+var common = require('../DAO/common');
 
 //收藏、取消收藏
 var user = {
-    login:function (user_tel,password,callback) {
-        var sql = "select id,nick_name,portrait,coin,integral,achievement_point,time_logon,tel,pay from t_user where tel=? and password=?";
-        query(sql,[user_tel,password],function (result) {
-            if(result.length>0){
-                return callback(result)
-            }else {
-                var sql = "select id,nick_name,portrait,coin,integral,achievement_point,time_logon,tel pay from t_user where nick_name=? and password=?";
-                query(sql,[user_tel,password],function (res) {
-                    return callback(res)
+    getUserInfo: function (obj, callback) {
+        var sqlUser = 'SELECT * FROM t_user WHERE only_id LIKE "%' + obj.user + '%" ';
+        query(sqlUser, [], function (userInfo) {
+            return callback(userInfo);
+        })
+    },
+    getBinding: function (obj, callback) {
+        var sqlUser = "SELECT * FROM t_user WHERE only_id=? OR tel=? OR nick_name=?";
+        query(sqlUser, [obj.user, obj.user, obj.user], function (userInfo) {
+            if (userInfo.length) {
+                var upSql = "UPDATE t_user SET is_inside=1, a_id=?";
+                query(upSql, [obj.id], function (result) {
+                    return callback(result);
                 })
+            } else {
+                return callback(userInfo);
             }
-        })
-    },
-    gameComment:function (userId,gameId,score,content,agree,addTime,parentId,address,callback) {
-        var sql="INSERT INTO t_game_recommend (user_id,game_id,score,content,agree,add_time,parent_id,address) values (?,?,?,?,?,?,?,?)";
-        query(sql,[userId,gameId,score,content,agree,addTime,parentId,address],function (result) {
-            return callback(result)
-        })
-    },
-    getGameCommentScoreById:function (game_id,callback) {
-        var  sql="select score from t_game_recommend where game_id=?";
-        query(sql,[game_id],function (result) {
-            return callback(result)
-        })
-    },
-    getUserCommentLen:function (game_id,user_id,callback) {
-        var  sql="select count(*) as count from t_game_recommend where game_id=? and user_id=?";
-        query(sql,[game_id,user_id],function (result) {
-            return callback(result)
-        })
-    },
-    updateGameScore:function (game_id,score,callback) {
-        var sql="UPDATE t_game SET grade=? where id=?";
-        query(sql,[score,game_id],function (result) {
-            return callback(result)
-        })
-    },
-    reg:function (tel,password,timeLogon,callback) {
-        var sqlUser="select * from t_user where tel =?";
-        var sql ="INSERT INTO t_user (nick_name,password,portrait,coin,integral,achievement_point,sign,time_unlock,time_logon,tel) values (?,?,0,0,500,0,0,0,?,?)";
-        query(sqlUser,[tel],function (result) {
-            if(result.length<=0){
-                query(sql,[tel,password,timeLogon,tel],function (res) {
-                    return callback(res);
-                })
-            }else {
-                return callback(result)
-            }
-        })
-    },
-    updateOnlyidById:function (id,callback) {
-        var onlyId=(parseInt(id)+123456);
-        var sql="UPDATE t_user SET only_id=? where id=?";
-        query(sql,[onlyId,id],function (result) {
-            return callback(result)
-        })
-    },
-    userList:function (callback) {
-        var sql='select nick_name,coin,integral,achievement_point,time_logon,tel,new_online,channel,sign,new_sign from t_user';
-        query(sql,[],function (result) {
-            return callback(result);
-        })
-    },
-    getChannel:function (callback) {
-        var sql="select * from t_channel";
-        query(sql,[],function (result) {
-            return callback(result)
-        })
-    },
-    updateChannel:function (channel,uid,callback) {
-        var sql="UPDATE t_user SET channel=? where id=?";
-        query(sql,[channel,uid],function (result) {
-                var sql = "select id,nick_name,portrait,coin,integral,achievement_point,sign,time_unlock,time_logon,tel,pay from t_user where id=?";
-                query(sql,[uid],function (res) {
-                    return callback(res)
-                })
-
-        })
-    },
-    selectUserIntegral:function (id,callback) {
-        var sql="select integral from t_user where id=?";
-        query(sql,[id],function (result) {
-            return callback(result)
-        })
-    },
-    updateUserIntegral:function (id,integral,callback) {
-        var sql="update t_user set integral=? where id=?";
-        query(sql,[integral,id],function (result) {
-            return callback(result)
-        })
-    },
-    selectUserCoin:function (id,callback) {
-        var sql="select coin from t_user where id=?";
-        query(sql,[id],function (result) {
-            return callback(result)
-        })
-    },
-    updateUserCoin:function (id,integral,callback) {
-        var sql="update t_user set Coin=? where id=?";
-        query(sql,[integral,id],function (result) {
-            return callback(result)
-        })
-    },
-    addLottery:function (uid,gty,num,start,end,value,callback) {
-        var sql="insert into t_lottery (user_id,gift_type,num,start_time,end_time,value) values (?,?,?,?,?,?)";
-        query(sql,[uid,gty,num,start,end,value],function (result) {
-            return callback(result);
-        })
-    },
-    updateLottery:function (uid,gty,callback) {
-        var sql="select * from t_lottery where user_id =? and gift_type=? LIMIT 1;";
-        query(sql,[uid,gty],function (result) {
-            if(result.length){
-                console.log(result);
-                var num=result[0].num+1;
-                var sql= "update t_lottery set num=? where user_id =? and gift_type=?";
-                query(sql,[num,uid,gty],function (result) {
-                    return callback(result)
-                })
-            }else {
-                var sql="insert into t_lottery (user_id,gift_type,num) values (?,?,1)";
-                query(sql,[uid,gty],function (result) {
-                    return callback(result)
-                })
-            }
-        })
-    },
-    getLotteryByUid:function (uid,callback) {
-        var sql="select * from t_lottery where user_id=?";
-        query(sql,[uid],function (result) {
-            return callback(result)
         })
     },
 
-    getServerAddress:function (callback) {
-        var sql="select * from t_server";
-        query(sql,[],function (result) {
-            return callback(result)
+    getFindUser: function (obj, callback) {
+        var sql = "SELECT * FROM t_user WHERE a_id=? AND is_inside=1";
+        query(sql, [obj.admin_id], function (isBinding) {
+            if (isBinding.length) {
+                return callback({state: -1, info: isBinding});
+            }
+
+            var sql = "SELECT * FROM t_user WHERE nick_name=? OR tel=?";
+            query(sql, [obj.nick_name, obj.tel], function (result) {
+                var state = 1;
+                if (result.length) {
+                    state = -2
+                }
+                return callback({state: state, info: result});
+            })
         })
+
+
     },
-    getCoinById:function (id,callback) {
-        var sql="select coin from t_user where id=?";
-        query(sql,[id],function (result) {
-            return callback(result)
-        })
-    },
-    getSignById:function (id,callback) {
-        var  sql = "select sign,new_sign from t_user where id=?";
-        query(sql,[id],function (result) {
-            return callback(result)
-        })
-    },
-    updateSign:function (id,sign,newSign,callbcak) {
-        var sql ="update t_user set sign=?,new_sign=? where id=?";
-        query(sql,[sign,newSign,id],function (result) {
-            return callbcak(result)
-        })
-    },
-    updateNickName:function (id,nickName,callback) {
-        var sql="update t_user set nick_name=? where id=?";
-        query(sql,[nickName,id],function (result) {
-            return callback(result)
-        })
-    },
-    updateHead:function (head,id,callback) {
-        var sql="update t_user set portrait=? where id=?";
-        query(sql,[head,id],function (result) {
-            return callback(result)
-        })
-    },
-    updateSex:function (id,sex,callback) {
-        var sql="update t_user set sex=? where id=?";
-        query(sql,[sex,id],function (result) {
-            return callback(result)
-        })
-    },
-    getUserMsgById:function (id,callback) {
-        var sql="select id,nick_name,portrait,coin,integral,tel,sign,new_sign,channel,sex,pay,only_id  from t_user where id=?";
-        query(sql,[id],function (result) {
-            return callback(result)
-        })
-    },
-    addAddress:function (uid,name,tel,area,detail,callback) {
-        var sql="insert into t_address (user_id,user_name,tel,area,detail_address) values (?,?,?,?,?)";
-        query(sql,[uid,name,tel,area,detail],function (result) {
+
+    getAddUser: function (obj, callback) {
+        var date = new Date();
+        var nowTime = date.getTime() / 1000
+        var month = date.getMonth() + 1
+        var day = date.getDate()
+        var H = date.getHours()
+        var M = date.getMinutes()
+
+        var nick_name = obj.nickName || "ONE_" + month + day + "_" + Math.floor(Math.random() * 99999);
+        // var pwd = common.pwdMd5(obj.password);
+
+        var sql = "INSERT INTO t_user (`nick_name`,`password`,`portrait`,`time_logon`,`tel`,`is_inside`,`a_id`) VALUES (?,?,?,?,?,1,?)";
+        query(sql, [nick_name, obj.password, obj.portrait, nowTime, obj.tel, obj.a_id], function (result) {
             return callback(result);
         })
+
     },
-    editAddress:function (uid,name,tel,area,detail,callback) {
-        var sql="update t_address set user_name=?,tel=?,area=?,detail_address=? where id=?";
-        query(sql,[name,tel,area,detail,uid],function (result) {
-            return callback(result)
-        })
-    },
-    getAddress:function (uid,callback) {
-        var sql="select * from t_address where user_id=?";
-        query(sql,[uid],function (result) {
+
+    getUpOnlyId: function (obj, callback) {
+        var onlyId = (parseInt(obj) + 123456);
+        var sql = "UPDATE t_user SET only_id=? where id=?";
+        query(sql, [onlyId, obj], function (result) {
             return callback(result)
         })
     }
